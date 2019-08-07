1b9266c1e06934e686e9a588f73430ac2440f399
Mark Rad <radman180@gmail.com>
2019-08-06 21:06:47 -0400

    NA|NA| added http call to github and displayed it on users page

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../user.service';
 
 @Component({
-  selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  selector: 'app-users-list',
+  template: `
+    <section class="section">
+    <div class="container">
+    <div class="columns is-multiline">
+
+      <!-- loop over our users and give them each a card -->
+      <div class="column is-4" *ngFor="let user of users | async">
+        <div class="card">
+          <div class="card-content">
+            {{ user.login }}
+          </div>
+        </div>
+      </div>
+
+    </div>
+    </div>
+    </section>
+  `,
+  styles: []
 })
+
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.users = this.userService.getUsers();
   }
 
 }
